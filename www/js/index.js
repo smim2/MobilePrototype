3c641ad3021eb1c3a1ae955edf3b89069a5eea62
smim2 <55838879+smim2@users.noreply.github.com>
2020-04-30 11:20:08 +0100

    Test

--- www/js/index.js
+++ www/js/index.js
@@ -1,35 +1,21 @@
-var latitude = 52.5586913;
-var longitude= -1.5282136;
-
-
-/*document.addEventListener("deviceready", onDeviceReady, false);
-    function onDeviceReady() {
-       	navigator.geolocation.getCurrentPosition(geolocationSuccess,[geolocationError]);
-	var onSuccess = function(position){
-	latitude = position.coords.latitude;
-	longitude = position.coords.longitude;
-};
-	function onError(error){
-	alert("Error" + error.code)
-}
-
-navigator.geolocation.getCurrentPosition(onSuccess, onError); 
-};
-*/
-    
-
-
-// Create the script tag, set the appropriate attributes
-var script = document.createElement('script');
-script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAy6qaNJOjP75X89IBmDmOYiYKr4qwqYKg&callback=initMap';
-script.defer = true;
-script.async = true;
-
-// Attach your callback function to the `window` object
-window.initMap = function() {
-  // JS API is loaded and available
-  
-} 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 var app = {
     // Application Constructor
     initialize: function() {
@@ -48,92 +34,16 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-		startmap();
     },
     // Update DOM on a Received Event
-    
-};	
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
 
-// Append the 'script' element to 'head'
-document.head.appendChild(script);
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
 
-
-
-//setting the locations for demo defib data
-var defib1;
-defib1 = {lat: 52.1912156, lng: -2.181245};
-
-var defib2;
-defib2 = {lat: 52.190033, lng: -2.2263984};
-
-var defib3;
-defib3 = {lat: 52.1937585, lng: -2.2260509};
-//setting the demo location to worcester
-var worc;
-worc = {lat: 52.192, lng: -2.220};
-
-
-
-
-	/*window.onload=*/function startmap(){
-    var mapDiv = document.getElementById("map_canvas");
-    var Userlatlng = new google.maps.LatLng(latitude, longitude);
-
-    var options = {
-      center: Userlatlng,
-      zoom: 12,
-      
-    };
-
-	
-	
-    var map = new google.maps.Map(mapDiv, options);
-//This code sets the center of the map to Worcester for demonstration purposes
-	
-	google.maps.event.addDomListener(document.getElementById('GoWorc'), 'click', function () {
-
-    map.setCenter(new google.maps.LatLng(52.192,-2.220));
-	
-	
-//Sets a new "Position" Marker in worc, for demo purposes	
-	var marker1 = new google.maps.Marker({
-      position: worc,
-      map: map,
-      title: 'Worcester',
-	  icon: iconbase2 + 'man.png'
-    });
-});
-//Linking to googles icon sheets
-	var iconBase1 = 'https://maps.google.com/mapfiles/kml/pal4/';
-	var iconbase2 = 'https://maps.google.com/mapfiles/kml/shapes/';
-	
-//creating the markers for the users position and defib locations using googles icons
-    var marker1 = new google.maps.Marker({
-      position: Userlatlng,
-      map: map,
-      title: 'User Location',
-	  icon: iconbase2 + 'man.png'
-    });
-	
-	var marker2 = new google.maps.Marker({
-		position: defib1,
-		map: map,
-		icon: iconBase1 + 'icon55.png'
-	});
-	
-		var marker3 = new google.maps.Marker({
-		position: defib2,
-		map: map,
-		icon: iconBase1 + 'icon55.png'
-	});
-	
-	var marker4 = new google.maps.Marker({
-		position: defib3,
-		map: map,
-		icon: iconBase1 + 'icon55.png'
-	});
-	
-
-}
-
-
+        console.log('Received Event: ' + id);
+    }
+};
